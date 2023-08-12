596b68e04faf266b5dd944d4ab0fa639643a53cf
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-08-12 21:39:17 +0100

    moved lxly and cctp into contracts folder

---
+++ contracts/contracts/cctp/Bridge.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+import "./interfaces/IMessageTransmitter.sol";
+import "./interfaces/ITokenMessenger.sol";
+import "./libraries/BridgeHelper.sol";
+
+contract CrossChainBridge {
+
+    uint32 public immutable CCTP_DOMAIN;
+
+    ISwapRouter public immutable swapRouter;
+    IERC20 public immutable usdcToken;
+    ITokenMessenger public immutable tokenMessenger;
+    IMessageTransmitter public immutable messageTransmitter;
+
+    mapping(address => bool) public isSupportedToken;
+    mapping(address => uint24) public tokenFee;
+    mapping(address => bool) public bridgeAdmins;
+
+    event BridgeDepositReceived(uint32 sourceChain, uint32 destinationChain, uint64 nonce, uint256 amount, address indexed from, address indexed recipient, address indexed destinationToken);
+    event BridgeWithdrawalMade(address indexed recipient, uint64 nonce, uint256 amount, address indexed token);
+
+    constructor(address[] memory supportedTokens, address swapRouterAddr, address usdcTokenAddr, address tokenMessengerAddr, address messageTransmitterAddr, uint32 domain) {
+        for (uint256 i = 0; i < supportedTokens.length; i++) {
+            isSupportedToken[supportedTokens[i]] = true;
+        }
+
+        swapRouter = ISwapRouter(swapRouterAddr);
+        usdcToken = IERC20(usdcTokenAddr);
+        tokenMessenger = ITokenMessenger(tokenMessengerAddr);
+        messageTransmitter = IMessageTransmitter(messageTransmitterAddr);
+        CCTP_DOMAIN = domain;
+        bridgeAdmins[msg.sender] = true;
+    }
+
+    modifier onlyAdmin(){
+        require(bridgeAdmins[msg.sender], "Not Permitted");
+        _;
+    }
+
+    function performSwap(address _tokenIn, address _tokenOut, address _recipient, uint256 amount) internal returns (uint256 amountOut) {
+        // Approve UNISWAP Router to spend token
+        TransferHelper.safeApprove(_tokenIn, address(swapRouter), amount);
+        // Swap The token for USDC
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: _tokenIn,
+            tokenOut: _tokenOut,
+            fee: tokenFee[_tokenIn],
+            recipient: _recipient,
+            deadline: block.timestamp,
+            amountIn: amount,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+
+    /**
+     * @notice Deposit an amount of a supported token to the bridge
+     * @param amount Amount of tokens to be deposited to the bridge
+     * @param sourceToken The supported token address to be deposited
+     * @param destinationToken The supported token on the destination chain that the user receives
+     * @param recipient The address to receive the destination token on the destination chain
+     * @param destinationDomain CCTP Domain identifier of the destination chain
+     * @param destinationContract Address of the contract on the destination chain where cctp sends the token
+    */
+    function deposit(uint256 amount, address sourceToken, address destinationToken, uint32 destinationDomain, address recipient, address destinationContract) public returns (uint64) {
+        require(isSupportedToken[sourceToken], "Source Token not supported");
+        require(isSupportedToken[destinationToken], "Destination Token not supported");
+
+        // Transfer the token from the caller to the bridge contract
+        TransferHelper.safeTransferFrom(sourceToken, msg.sender, address(this), amount);
+        uint256 amountOut = amount;
+
+        address usdcAddress = address(usdcToken);
+        if (sourceToken != usdcAddress) {
+            amountOut = performSwap(sourceToken, usdcAddress, address(this), amount);
+        }
+
+        // Approve Token Messenger to Spend the swapped amount
+        TransferHelper.safeApprove(address(usdcToken), address(tokenMessenger), amountOut);
+        // Move the USDC To CCTP Contract
+        uint64 nonce = tokenMessenger.depositForBurn(amountOut, destinationDomain, BridgeUtil.addressToBytes32(destinationContract), address(usdcToken));
+        emit BridgeDepositReceived(CCTP_DOMAIN, destinationDomain, nonce, amountOut, msg.sender, recipient, destinationToken);
+        return nonce;
+    }
+
+    /**
+     * @notice Method to add token to the list of supported tokens. Only admin can call this method
+     * @param token Address of the token
+     * @param fee UNISWAP Fee TIER for swaps
+    */
+    function addToken(address token, uint24 fee) public onlyAdmin {
+        isSupportedToken[token] = true;
+        tokenFee[token] = fee;
+    }
+
+    /**
+     * @notice Method to add admin to the bridge contract
+     * @param _admin Address of the admin
+    */
+    function addAdmin(address _admin) public onlyAdmin {
+        bridgeAdmins[_admin] = true;
+    }
+
+    /**
+     * @notice Method to recieve tokens and transfer to the recipient on the destination chain. Only a contract admin can call this method
+     * @param message cctp contract message from source chain
+     * @param signature attestation from cctp attestation API for the message
+     * @param nonce message nonce from cctp contract
+     * @param amount Amount of destination token to be sent to recipient
+     * @param destinationToken Token on the recipient receives
+     * @param recipientAddress address of the recipient
+    */
+    function sendToRecipient(bytes calldata message, bytes calldata signature, uint64 nonce, uint256 amount, address destinationToken, address recipientAddress) public onlyAdmin {
+        require(messageTransmitter.receiveMessage(message, signature), "Receive Message Failed");
+        uint256 amountOut = amount;
+        if (destinationToken != address(usdcToken)) {
+            amountOut = performSwap(address(usdcToken), destinationToken, recipientAddress, amount);
+        } else {
+            usdcToken.transfer(recipientAddress, amountOut);
+        }
+        emit BridgeWithdrawalMade(recipientAddress, nonce, amountOut, destinationToken);
+    }
+}
