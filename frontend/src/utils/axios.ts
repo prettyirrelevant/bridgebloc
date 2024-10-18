a6338da67bd5b5cb9b378a962fce714cff073aba
Isaac Adewumi <ienioladewumi@gmail.com>
2024-10-18 16:01:42 +0100

    update rpc & server base url

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,5 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL =
-  "https://bridgebloc-api-eb9bd3c3ed18.herokuapp.com/api/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.baseURL = 'https://bridgebloc-api.up.railway.app/api/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
--- frontend/src/wagmi-setup.ts
+++ frontend/src/wagmi-setup.ts
@@ -1,39 +1,70 @@
-import { configureChains, createConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-import { infuraProvider } from "wagmi/providers/infura";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { configureChains, createConfig, type Chain } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { InjectedConnector } from 'wagmi/connectors/injected';
 
 import {
-  goerli,
+  sepolia,
   mainnet,
   polygon,
+  optimism,
+  optimismSepolia,
   arbitrum,
-  avalanche,
-  polygonZkEvm,
-  avalancheFuji,
-  polygonMumbai,
-  arbitrumGoerli,
-  polygonZkEvmTestnet,
-} from "wagmi/chains";
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+} from 'wagmi/chains';
+
+export const polygonAmoy = {
+  id: 80_002,
+  name: 'Polygon Amoy',
+  network: 'amoy',
+  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-amoy.polygon.technology'],
+    },
+    public: {
+      http: ['https://polygon-amoy.gateway.tenderly.co'],
+    },
+    alchemy: {
+      http: ['https://polygon-amoy.g.alchemy.com/v2'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'PolygonScan',
+      url: 'https://amoy.polygonscan.com',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 3127388,
+    },
+  },
+  testnet: true,
+} as const satisfies Chain;
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    goerli,
+    sepolia,
     mainnet,
     polygon,
+    polygonAmoy,
     arbitrum,
-    avalanche,
-    polygonZkEvm,
-    avalancheFuji,
-    polygonMumbai,
-    arbitrumGoerli,
-    polygonZkEvmTestnet,
+    base,
+    baseSepolia,
+    arbitrumSepolia,
+    optimism,
+    optimismSepolia,
   ],
 
   [
-    infuraProvider({ apiKey: import.meta.env.VITE_INFURA_API_KEY! }),
+    // infuraProvider({ apiKey: import.meta.env.VITE_INFURA_API_KEY! }),
+    alchemyProvider({ apiKey: import.meta.env.VITE_ALCHEMY_API_KEY! }),
     publicProvider(),
-  ]
+  ],
 );
 
 export const config = createConfig({
@@ -42,7 +73,7 @@ export const config = createConfig({
     new InjectedConnector({
       chains,
       options: {
-        name: "Injected",
+        name: 'Injected',
         shimDisconnect: true,
       },
     }),
